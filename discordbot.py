f13d54c64c3d25725bb88e9bc5b7bd7d3b7aabc6
yukina018 <87555963+yukina018@users.noreply.github.com>
2021-08-09 21:56:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -46,5 +46,52 @@ async def on_message(ctx):
 
     await bot.process_commands(ctx)
     
-        
+
+import requests
+import json
+
+webhook_url = 'https://discord.com/api/webhooks/874273799418310657/SFQ9K9zswagU_xCSXmj9fLS38yC35_XcTFl_fnBvEJljWPalAkVO_Wjx92Imxon7Q20e'
+
+main_content = {
+  "username": "公式情報BOT",
+  "avatar_url": "https://i.imgur.com/4M34hi2.png",
+  "content": "Text message. Up to 2000 characters.",
+  "embeds": [
+      "title": "Title",
+      "url": "https://twitter.com/assaultlily_lb,
+      "description": "Text message. You can use Markdown here. *Italic* **bold** __underline__ ~~strikeout~~ [hyperlink](https://google.com) `code`",
+      "color": 15258703,
+      "fields": [
+        {
+          "name": "Text",
+          "value": "More text",
+          "inline": True
+        },
+        {
+          "name": "Even more text",
+          "value": "Yup",
+          "inline": True
+        },
+        {
+          "name": "Use `\"inline\": true` parameter, if you want to display fields in the same line.",
+          "value": "okay..."
+        },
+        {
+          "name": "Thanks!",
+          "value": "You're welcome :wink:"
+        }
+      ],
+      "footer": {
+        "text": "Woah! So cool! :smirk:",
+        "icon_url": "https://i.imgur.com/fKL31aD.jpg"
+      }
+    }
+  ]
+}
+
+
+requests.post(webhook_url,json.dumps(main_content),headers={'Content-Type': 'application/json'})
+    
+    
+    
 bot.run(token)
